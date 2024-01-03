486697e2f929b16aed0a4250171257d729564093
ustav <gustavobreasy@gmail.com>
2024-01-03 17:01:39 -0300

    add y/o

--- src/app/components/greeting.tsx
+++ src/app/components/greeting.tsx
@@ -9,7 +9,7 @@ export default function Greeting() {
         <div className="lg:flex flex-row sm:inline">
             <div className="lg:w-1/2 mt-24 mb-24 lg:mt-5 lg:mb-0">
                 <h1 className="text-3xl text-center">Hello! you can call me ustav 🤙</h1>
-                <h2 className="text-xl text-center">I'm a Brazilian front-end developer but also a <a href="" className="underline">chesmitry and design learner</a> </h2>
+                <h2 className="text-xl text-center">I'm a Brazilian 16 y/o front-end developer but also a <a href="" className="underline">chesmitry and design learner</a> </h2>
             </div>
             <div className="lg:w-1/2 p-3 mt-10 lg:mt-5">
                 <Lanyard />
