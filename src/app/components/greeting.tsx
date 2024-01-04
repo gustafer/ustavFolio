b92442891ae96de5063d6dcbedc9150825aeee11
ustav <gustavobreasy@gmail.com>
2024-01-04 20:17:32 -0300

    add toggleTheme switch to another component and chesmistry is spelt wrong lol

--- src/app/components/greeting.tsx
+++ src/app/components/greeting.tsx
@@ -9,7 +9,7 @@ export default function Greeting() {
             <div className="lg:w-1/2 mt-24 mb-24 lg:mt-5 lg:mb-0">
                 <div className="py-1 lg:py-24">
                     <h1 className="text-3xl text-center">Hello! you can call me ustav 🤙</h1>
-                    <h2 className="text-2xl text-center">I'm a Brazilian 16 y/o front-end developer but also a <a href="" className="underline">chesmitry and design learner</a> </h2>
+                    <h2 className="text-2xl text-center">I'm a Brazilian 16 y/o front-end developer but also a <a href="" className="underline">chemistry and design learner</a> </h2>
                 </div>
             </div>
             <div className="lg:w-1/2 p-3 mt-10 lg:mt-5 ">
